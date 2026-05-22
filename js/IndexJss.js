de38c86136362337b6f1aa357732d2f742078931
Jon Linaje <jlinaje@abakandoxr.com>
2026-05-22 15:52:39 +0200

    PortfolioSlider

--- js/IndexJss.js
+++ js/IndexJss.js
@@ -1,16 +1,49 @@
-const arrow = document.querySelectorAll(".portfolio_arrow");
+const arrowBack = document.getElementById("arrowBack");
+const arrowNext = document.getElementById("arrowNext");
 const portfolio = document.querySelectorAll(".portfolio-list-item");
 console.log(portfolio);
-//Slider of my portfolios
 
-for (let i = 0; i < 2; i++) {
-    arrow[i].addEventListener("click", function () {
-        var hider = $('#hider');
-        if(hider.css('visibility') == 'visible') {
-            hider.css('visibility', 'hidden');
+//Slider of my portfolios
+let currentIndex = 0;
+function showSlider(index) {
+    for (let i = 0; i < portfolio.length; i++) {
+        portfolio[i].classList.add("disable");
+    }
+    portfolio[index].classList.remove("disable");
+}
+
+function slider(direction) {
+    if (direction === 1) {
+        currentIndex = (currentIndex + 1) % portfolio.length;
+    } else if (direction === -1) {
+        currentIndex = currentIndex - 1;
+        if (currentIndex < 0) {
+            currentIndex = portfolio.length - 1;
         }
-        else {
-            hider.css('visibility', 'visible');
-        }
-        ctx.clearRect(0, 0, 100, 100);
-})};
+    }    
+    showSlider(currentIndex);
+}
+
+showSlider(currentIndex);
+arrowNext.addEventListener("click", () => {
+    slider(1);
+    stopAutoplay(null);
+});
+arrowBack.addEventListener("click", () => {
+    slider(-1);
+    stopAutoplay(null);
+});
+
+//Autoplay for the slider
+let autoplayInterval = null;
+function startAutoplay(interval) {
+   stopAutoplay();
+   autoplayInterval = setInterval(() => {
+      slider(1);
+   }, interval);
+}
+startAutoplay(4000);
+
+function stopAutoplay() {
+   clearInterval(autoplayInterval);
+}
